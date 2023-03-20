4547adce61d63add0c6bae1d19bf1a7e17ee4337
root <root@gpu-cloud-vnode20.dakao.io>
2023-03-20 18:03:00 +0900

    add layers

--- layers/scaled_dot_product_attention.py
+++ layers/scaled_dot_product_attention.py
@@ -4,5 +4,13 @@ from torch import nn
 class ScaledDotProductAttention(nn.Module) :
     
     def __init__(self) :
-        super().__init__()
+        super(ScaledDotProductAttention, self).__init__()
         self.softmax = nn.Softmax(dim = -1)
+    
+    def forward(self, q, k, v, mask=None, e=1e-12) :
+        
+        #input은 4차원 tensor [256, ]
+        batch_size, head, length, d_tensor = k.size() 
+        
+        # 1. Matmul
+        score = q @ k.transpose
